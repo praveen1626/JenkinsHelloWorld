58afdd43d9a61a93c4e251cb41ff8df6e8284206
praveen1626 <paveefunny@gmail.com>
2020-03-11 08:08:59 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,10 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World Made some changes!");
+		System.out.println("Hello World Trying to connect webhook!");
 		System.out.println(24+26);
 		System.out.println(152-24);
 		System.out.println(15*15);
+		System.out.println(19*19);
 	}
 }
